79cb60d7da484abdc2a47d538e32d4fdeb1a713c
Anand <anandmaurya542@gmail.com>
2022-06-22 23:04:39 +0530

    Practicals Swing files added

---
+++ practice/FrameUsingSwing.java
@@ -0,0 +1,42 @@
+ //Objective
+//        Write a program to create a frame using Swing. Implement mouseCliccked(),
+//        mouseEntered() and mouseExited() events.  Frame should become visible when mouse enters it.
+
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+class Mainf {
+    JLabel l;
+    Mainf() {
+        JFrame f = new JFrame("Swing Frame");
+        l = new JLabel("", SwingConstants.CENTER);
+        l.setBounds(15, 110, 260, 30);
+        f.add(l);
+        f.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                l.setText("Mouse Clicked");
+            }
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                l.setText("Mouse Entered");
+            }
+            @Override
+            public void mouseExited(MouseEvent e) {
+                l.setText("Mouse Exited");
+            }
+        });
+        f.setSize(300, 300);
+        f.setLayout(null);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new Mainf();
+            }
+        });
+    }
+}
---
+++ practice/SwingBackground.java
@@ -0,0 +1,29 @@
+//Objective
+//        Using Swing, write a program to display a string in frame
+//        window with pink colour as background.
+
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+class MainS {
+    JLabel l;
+    MainS() {
+        JFrame f = new JFrame("Swing Pink");
+        l = new JLabel("This is some text", SwingConstants.CENTER);
+        l.setBounds(15, 15, 260, 30);
+        f.add(l);
+        f.getContentPane().setBackground(Color.PINK);
+        f.setSize(300, 100);
+        f.setLayout(null);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new MainS();
+            }
+        });
+    }
+}
